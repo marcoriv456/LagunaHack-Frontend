4d80cb86812090ee81f18d5db6542cb28baba672
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-02-09 13:40:46 -0600

    feat: Add scroll efectos to sponsors page

--- src/app/ui/pages/sponsors/sponsors.page.ts
+++ src/app/ui/pages/sponsors/sponsors.page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Gallery } from '../../atoms/gallery/gallery';
 import { Text } from '../../atoms/text/text';
 import { Title } from '../../atoms/title/title';
+import { Intersectable } from '../../directives/intersectable/intersectable';
 
 @Component({
   selector: 'hack-sponsors',
-  imports: [Title, Text, Gallery],
+  imports: [Title, Text, Gallery, Intersectable],
   templateUrl: './sponsors.page.html',
   styleUrl: './sponsors.page.css',
 })
